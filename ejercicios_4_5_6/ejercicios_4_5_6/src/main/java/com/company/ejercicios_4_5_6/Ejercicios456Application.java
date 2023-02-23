9f69b02d801a2db25317c501f3f86aa1baa33841
Marco Carcamo <macdevg@gmail.com>
2023-02-23 00:02:24 -0600

    Clase 6 completada y creación del proyecto para ejercicios 4, 5 y 6

---
+++ ejercicios_4_5_6/ejercicios_4_5_6/src/main/java/com/company/ejercicios_4_5_6/Ejercicios456Application.java
@@ -0,0 +1,13 @@
+package com.company.ejercicios_4_5_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicios456Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicios456Application.class, args);
+	}
+
+}
--- ob-rest-datajpa/ob-rest-datajpa/src/main/java/com/example/obrestdatajpa/controller/BookController.java
+++ ob-rest-datajpa/ob-rest-datajpa/src/main/java/com/example/obrestdatajpa/controller/BookController.java
@@ -2,11 +2,12 @@ package com.example.obrestdatajpa.controller;
 
 import com.example.obrestdatajpa.entity.Book;
 import com.example.obrestdatajpa.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BookController {
@@ -32,8 +33,32 @@ public class BookController {
     }
 
     //Buscar un solo libro en base de datos según su id
+    @GetMapping("api/books/{id}")
+    public ResponseEntity <Book> findOneById(@PathVariable Long id) {
+
+        Optional<Book> bookOpt = bookRepository.findById(id);
+        // opción
+        if (bookOpt.isPresent()){
+            return ResponseEntity.ok(bookOpt.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+        // Opción 2
+        //return bookOpt.orElse(null);
+
+
+    }
 
     //Crear un nuevo libro en base de datos
+    @PostMapping("/api/books")
+    public Book create(@RequestBody Book book, @RequestHeader HttpHeaders headers){
+        System.out.println(headers.get("User-Agent"));
+        //Guardar el libro por parametro es la base de datos
+        return bookRepository.save(book);
+    }
+
+
 
     //Actualizar un libro existente en base de datos
 
